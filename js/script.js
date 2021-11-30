47827d034cba31d53a181d10ed56979f0b48e763
turchimatteo <turchi12@gmail.com>
2021-11-30 15:48:50 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* 
+Visualizzare con degli alert 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+*/
