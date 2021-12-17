e4c882f12cb3480882c2c2a151e526b1d1e725ce
turchimatteo <turchi12@gmail.com>
2021-12-17 14:27:12 +0100

    da rivedere perché non stampa i numeri

--- js/script.js
+++ js/script.js
@@ -7,3 +7,55 @@ Consigli del giorno:
 * Dividete in piccoli problemi la consegna.
 * Individuate gli elementi di cui avete bisogno per realizzare il programma.
 */
+const numbers = 5;
+const randomNumbers = getRandomNumbers(numbersInGame);
+
+//alert
+randomNumbers.forEach((alertNumber) =>{
+    alert(alertNumber)
+})
+
+//inizio gioco
+setTimeout(start, 3000);
+
+function start() {
+    const userArray = [];
+    const correctNumbers = [];
+    const wrongNumbers = [];
+    for (let i = 0; i < numbersInGame; i++) {
+        const userNumbers = parseInt(prompt('Scrivi il numero che hai visto'));
+        userArray.push(userNumbers);
+    }
+    console.log(userArray);
+    userArray.forEach((alertNumber) => {
+        if(randomNumbers.includes(alertNumber) && !correctNumbers.includes(alertNumber)){
+            correctNumbers.push(alertNumber);
+        } else{
+            wrongNumbers.push(alertNumber);
+        }
+    })
+    console.log(correctNumbers);
+    console.log(wrongNumbers);
+    if (!correctNumbers.length === 1) {
+        alert(`I numeri che hai scritto, ${correctNumbers}, sono giusti! Ne hai indovinari ${correctNumbers.length}`)
+    }else {
+        alert(`I numeri che hai scritto, ${correctNumbers}, sono sbagliati! Ne hai indovinari ${correctNumbers.length}`)
+    }
+}
+
+//funzione per numeri casuali
+function getRndInteger() {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//funzione array numeri casuali
+function getRandomNumbers (numberOfItems){
+    const numbersArray = [];
+    while(numbersArray.length < numberOfItems){
+        const newRandomNumber = getRndInteger (1, 100);
+        console.log(newRandomNumber)
+        if(!numbersArray.includes(newRandomNumber)){
+            numbersArray.push(newRandomNumber)
+        }
+    }   
+    return numbersArray;
+}
